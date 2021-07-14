6cc9a1b30415e7d23f917d0a9867da4376bb5253
augustoibarrola <augusto.t.ibarrola@gmail.com>
2021-07-14 10:45:45 -0400

    changed hero repository from crudrepository to jparepository for added functionality

--- src/main/java/com/heroes/repository/HeroRepository.java
+++ src/main/java/com/heroes/repository/HeroRepository.java
@@ -3,11 +3,12 @@ package com.heroes.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.heroes.entity.Hero;
 
-public interface HeroRepository extends CrudRepository<Hero, Integer> {
+public interface HeroRepository extends JpaRepository<Hero, Integer> {
 	
 //	Optional<List<Hero>> getAllHeroes();
 
